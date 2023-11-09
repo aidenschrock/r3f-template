28d7a33ad211e2a586e61dd2fe517319f0511dd2
Aiden Schrock <aidennoel@proton.me>
2023-11-09 15:32:55 -0600

    Revert "moved from Next to React+Vite"
    This reverts commit 34ee8a2d8e61226c1ab39846652168c7a916597f.

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
 });
